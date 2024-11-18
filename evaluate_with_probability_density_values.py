fixed the recommanded portion & added the cdf analysis codes and mu covariance same range

evaluate_with_probability_density_values.py
@@ -1,26 +1,20 @@
 from scipy.stats import multivariate_normal
+import numpy as np
 
 from grid_by_grid_guassian_estimation import grid_by_grid_displacement_observation
 
-def calculate_pdf_obj(curr_obj_pos,mu,cov_matrix):
-    """
-    this creates a list for the a object's pdf values for one grid cell
-    it traverses the list of all the displacements of one object in one grid cell calculates the multivariate_normal using numpy library functions
-    Parameters:
-    - curr_obj_pos: a list of displacement of one object on a particular grid
-    - mu: list of means for dx,dy 
-    - covariance: 2*2 list 
-    Returns:
-    - pdfs_for_obj: list of pdf values for one object's one grid cell.
-    """
-    
-    pdfs_for_obj=[]    
-    for point_to_evaluate in curr_obj_pos:
-        #print(f"from calculate pdf of objects function: {point_to_evaluate}\n")
-        pdf_value = multivariate_normal.pdf(point_to_evaluate, mean=mu, cov=cov_matrix)
-        pdfs_for_obj.append(pdf_value)
-        #print(f"pdf value is: {pdf_value}, list is: {pdfs_for_obj}\n")
-    return pdfs_for_obj
+def mismatching_pdf_observations(curr_obs,curr_pdf):
+    i=0;
+    for obj_id in curr_obs.keys() & curr_pdf.keys():
+        obs_sizes=curr_obs[obj_id]
+        pdf_sizes=curr_pdf[obj_id]
+        
+        #print(f"for {obj_id}: {obs_sizes,pdf_sizes}\n")
+        #print(f"for {obj_id}: {len(obs_sizes),len(pdf_sizes)}\n")
+        if(len(obs_sizes)-1!=len(pdf_sizes)):
+            print(f"for {obj_id}: {len(obs_sizes),len(pdf_sizes)}")
+            i+=1
+    print(i)
 
 
 def grid_by_grid_pdf(grid_stat, current_obj_dis):
@@ -31,7 +25,7 @@ def grid_by_grid_pdf(grid_stat, current_obj_dis):
     - grid_stat: 5*5 list of mu and covariance_matrix
     - curr_obj_dis: 5*5 list of displacement of one object
     Returns:
-    - flatten_pdfs: list of pdf values.
+    - pdfs_for_cells: list of pdf values.
     """
     pdfs_for_cells=[]
     for i, (stats, obj_dis) in enumerate(zip(grid_stat,current_obj_dis)):
@@ -47,13 +41,17 @@ def grid_by_grid_pdf(grid_stat, current_obj_dis):
             '''
             
             if(len(curr_obj_dis_cord)>0):
-                pdfs=calculate_pdf_obj(curr_obj_dis_cord,mu,cov_matrix)
-                pdfs_for_cells.append(pdfs)
-                #print(pdfs)
-    flatten_pdfs=[pdfs for sublist in pdfs_for_cells for pdfs in sublist]
-    return flatten_pdfs
+                curr_dis_arr = np.array(curr_obj_dis_cord)
+                mvn = multivariate_normal(mean=mu, cov=cov_matrix)
+                curr_pdf_values=mvn.pdf(curr_dis_arr)
+                curr_pdf_values = np.atleast_1d(curr_pdf_values)#converting to 1d array
+                
+                pdfs_for_cells.extend(curr_pdf_values)
+                
+                #print(f" calculated with numpy array:{curr_pdf_values.shape} {curr_pdf_values}")
+    return pdfs_for_cells
     
-def calculate_pdf_all_by_displacements(obs,grid_stats):
+def calculate_pdf_all_by_displacements(obs,grid_stats,max_x,max_y):
     """
     this creates a dictionary for all the pdf value for the displacements
     it takes one object observation at one time sends the dictionary (current_item_dict)to grid_by_grid_displacement_observation function to get the displacements of the objects
@@ -61,26 +59,33 @@ def calculate_pdf_all_by_displacements(obs,grid_stats):
     Parameters:
     - obs: dictionary containing {object id: [(frame,x_coord,y_coord)]}
     - grid_stats: M*N list of mu and covariance matrices
+    - max_x : x_coordinate maximum range
+    - max_y : y_coordinate maximum range
     Returns:
     - pdf_all_dict: dictionary {object_id: [pdf values]}. Each pdf value
       corresponds to one displacement.
     """
     pdf_all_dict = {}
     i=0
+    
+    MAX_X=max_x
+    MAX_Y=max_y
+    grid_squares=len(grid_stats)
+   
     for obj_id, obj_cord_list in obs.items():
         # Create a dictionary with the current object's ID and coordinates list
         current_item_dict = {obj_id: obj_cord_list}
         #print(f"from calculate all displacements functions: current object id: {obj_id}")
         
         # Compute displacement observations for the current object
-        curr_obj_dis = grid_by_grid_displacement_observation(current_item_dict)
+        curr_obj_dis = grid_by_grid_displacement_observation(current_item_dict,grid_squares,MAX_X,MAX_Y)
         
         # Calculate Pdfs by grid for the current object's displacement observations
         pdfs_by_grid = grid_by_grid_pdf(grid_stats, curr_obj_dis)
-        
+        #print(pdfs_by_grid)
         # Store the result in dead_pdf_all_dict
         pdf_all_dict[obj_id] = pdfs_by_grid
-
+        
     return pdf_all_dict
 
 def get_pdf_value_list(curr_pdf):

grid_by_grid_guassian_estimation.py
@@ -2,6 +2,27 @@ import re
 import collections
 import math
 import numpy as np
+import logging
+import os
+
+LOG_DIRECTORY = r"D:\RA work Fall2024\grid_by_grid_dead_alive\log_info"
+LOG_FILE = "gaussian_estimation.log"
+
+# Ensure the directory exists
+os.makedirs(LOG_DIRECTORY, exist_ok=True)
+
+# Full path to the log file
+LOG_PATH = os.path.join(LOG_DIRECTORY, LOG_FILE)
+
+# Configure logging globally
+logging.basicConfig(
+    level=logging.WARNING,  # Set the minimum level globally
+    format="%(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(),  # Log to the console
+        logging.FileHandler(LOG_PATH)  # Log to the specified file
+    ]
+)
 
 def load_observations(filename):
     """
@@ -42,7 +63,7 @@ def load_observations(filename):
     return observations
  
 
-def grid_by_grid_displacement_observation(curr_obs):
+def grid_by_grid_displacement_observation(curr_obs,grid_squares,max_x,max_y):
     """
     this function creates a 5*5 list (grid_obs) by processing a dictionary of observations where each grid cell contains the displacements
     the calculation of displacement across 2 axes is displacmenet along dx= x2-x1; dy=y2-y1
@@ -51,17 +72,19 @@ def grid_by_grid_displacement_observation(curr_obs):
     
     Parameters:
     - curr_obs: dictionary containing object's observations(object id: (frame,x_cordinate,y_coordinate))
-    
+    - grid_square: range for grid search
+    - max_x : x_coordinate maximum range
+    - max_y : y_coordinate maximum range
     Returns:
     - returns a 5*5 list of the displacements(dx,dy).
     """
-
-    # FIXME - make these parameters
-    MAX_X=2000
-    MAX_Y=4000
     
-    # FIXME - make this a parameter
-    GRID_SQUARES = 5
+    # FIXME - make these parameters(w)
+    MAX_X=max_x
+    MAX_Y=max_y
+    
+    # FIXME - make this a parameter(w)
+    GRID_SQUARES = grid_squares
 
     grid_obs = [[[] for _ in range(GRID_SQUARES)] for _ in range(GRID_SQUARES)]
     #print(obs) 
@@ -74,15 +97,15 @@ def grid_by_grid_displacement_observation(curr_obs):
             x1_frame, x1, y1 = obs[j]
             x2_frame, x2, y2 = obs[j+1]
             
-            # FIXME - don't use 4000/2000 constants; use MAX_X and MAX_Y
-            x1_row=math.floor(x1/4000*GRID_SQUARES)
-            y1_col=math.floor(y1/2000*GRID_SQUARES)
+            # FIXME - don't use 4000/2000 constants; use MAX_X and MAX_Y(w)
+            x1_row=math.floor(x1/MAX_X*GRID_SQUARES)
+            y1_col=math.floor(y1/MAX_Y*GRID_SQUARES)
 
-            x2_row=math.floor(x2/4000*GRID_SQUARES)
-            y2_col=math.floor(y2/2000*GRID_SQUARES)
+            x2_row=math.floor(x2/MAX_X*GRID_SQUARES)
+            y2_col=math.floor(y2/MAX_Y*GRID_SQUARES)
             #print(f"current cords are: {x1,y1}, {x2,y2} and row,col is: {x1_row,y1_col}")
             
-            # FIXME - if the below "if" is not true, then we are losing data;
+            # FIXME - if the below "if" is not true, then we are losing data;(logging to a log file)
             # this should be an error (rather than silently ignored)
             if(0<= x1_row <GRID_SQUARES and 0<= y1_col <GRID_SQUARES):
                 grid_pos=grid_obs[x1_row][y1_col]
@@ -99,10 +122,12 @@ def grid_by_grid_displacement_observation(curr_obs):
                         dy=(y2-y1)/frame_distance
                         grid_pos.append((dx,dy))
                         #print(f"skipping frame is: {x1_frame} , {dx,dy}")
-
+            else:
+                #print(f"missing data for the grid, for grid[{x1_row}][{y1_col}] for {x1,y1}.")
+                logging.error(f"missing data for the grid, for grid[{x1_row}][{y1_col}] for {x1,y1}.")
+                
         #assert loop_count == len(obs)-1, f"Loop count ({loop_count}) does not match list size ({len(obs)})"
-        #print(f"The loop ran the same number of times as the list size, loop count is: {loop_count}")
-        #print(missing_grid_obs)           
+        #print(f"The loop ran the same number of times as the list size, loop count is: {loop_count}")          
     return grid_obs
 
 def grid_covariance_calculate(grid_displacements):
@@ -117,19 +142,20 @@ def grid_covariance_calculate(grid_displacements):
     - returns a 5*5 list of mu and covariance matrix.
     """
 
-    # FIXME - make GRID_SQUARES into rows and columns, based on the shape of
+    # FIXME - make GRID_SQUARES into rows and columns, based on the shape of(w)
     # grid_displacements
-    GRID_SQUARES = 5
-    grid_stats = [[None for _ in range(GRID_SQUARES)] for _ in range(GRID_SQUARES)]
+    grid_stats = [[None for _ in range(len(row))] for row in grid_displacements]
+    flag=False
     
-    for i in range(GRID_SQUARES):
-        for j in range(GRID_SQUARES):
-            # FIXME - this if statement should always be true; if it's not, we
-            # don't have enough data to estimate mu/sigma for this cell. In
-            # fact, we should have at least 10, preferably 30 examples per cell.
+    for i in range(len(grid_displacements)):
+        for j in range(len(grid_displacements[i])):
+            # FIXME - this if statement should always be true; if it's not, we don't have enough data to estimate mu/sigma for this cell. (w)
+            # In fact, we should have at least 10, preferably 30 examples per cell.
             # If we don't, we don't have enough data - and that should at least
-            # be a warning, if not an error.
+            # be a warning, if not an error.(w)
             if(len(grid_displacements[i][j])>1):
+                if(len(grid_displacements[i][j])<30):
+                    logging.warning(f"grid[{i}][{j}] has less than 30 observations.")
                 dxdy_items = np.array(grid_displacements[i][j])
                 #print(f" for {i,j} cell displacements are {dxdy_items}, {dxdy_items.shape}")
                 #print(f" at {i,j} cell")
@@ -138,13 +164,17 @@ def grid_covariance_calculate(grid_displacements):
                 
                 cov_matrix = np.cov(dxdy_items.T)
                 #print(cov_matrix,cov_matrix.shape)
-                
+                #logging.warning(f"not enough data to calculate mu & sigma.")
+            else:
+                flag=True
+                logging.error(f"grid[{i}][{j}] doesnot enough data to calculate mu & sigma for grid[{i}][{j}].")
             # FIXME - this can set mu/cov_matrix based on a previous iteration
             # if the above if statement is false
-            grid_stats[i][j] = {
-                'mu': mu,
-                'cov_matrix': cov_matrix
-        }
+            if flag==False:
+                grid_stats[i][j] = {
+                    'mu': mu,
+                    'cov_matrix': cov_matrix
+                }
                       
     #print(grid_stats)
     return grid_stats 
@@ -160,9 +190,12 @@ def print_grid_stats(grid_stat):
     """
     for i, row_item in enumerate(grid_stat):
         for j, col_item in enumerate(row_item):
-            mu=col_item['mu']
-            cov_matrix=col_item['cov_matrix']
-            print(f"grid[{i}][{j}]: ")               
-            print(f"    mu: {mu}")              
-            print(f"    cov_matrix:\n{cov_matrix}")
+            if col_item is not None:
+                mu=col_item['mu']
+                cov_matrix=col_item['cov_matrix']
+                print(f"grid[{i}][{j}]: ")               
+                print(f"    mu: {mu}")              
+                print(f"    cov_matrix:\n{cov_matrix}")
+            else:
+                print(f"grid[{i}][{j}]: None")
  

result_visualization.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from collections import Counter
 from scipy.stats import multivariate_normal
+from matplotlib.patches import Ellipse
+import matplotlib as mpl
+mpl.rcParams['figure.max_open_warning'] = 100 
 
 def plot_pdf_histogram_bins(curr_pdf_list,object_type):
     
@@ -76,36 +79,185 @@ def object_plotting_with_pdf(pdf_with_cord):
     # Show the plot
     plt.show()
     
-def contour_plot(grid_stat,grid_dis,grid_obs):
+def mean_covariance_plot(grid_stat):
+    # Step 1: Compute global range for all plots
+    global_min_x, global_max_x = float('inf'), float('-inf')
+    global_min_y, global_max_y = float('inf'), float('-inf')
 
-    pdfs_with_cords=[]
-    for i, (stats, obj_dis,obj_cord) in enumerate(zip(grid_stat,grid_dis,grid_obs)):
-        for j, (stat_val,obj_dis_pos,obj_pos) in enumerate(zip(stats, obj_dis,obj_cord)):
-            mu=stat_val['mu']
-            cov_matrix=stat_val['cov_matrix']
-            curr_obj_dis_cord=obj_dis_pos if obj_dis_pos else []
-            curr_obj_pos_cord=obj_pos if obj_pos else []
+    # Iterate through the grid to find the global range
+    for row_item in grid_stat:
+        for col_item in row_item:
+            mu = col_item['mu']
+            cov_matrix = col_item['cov_matrix']
+            eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+            width, height = 2 * np.sqrt(eigenvalues)
+            max_range = max(width, height) * 1.5
             
-             
-            #print(f"at grid [{i}][{j}]")
-            #print(f"    mu: {mu}")              
-            #print(f"    cov_matrix:\n{cov_matrix}")
-            #print(f"displacements list lenght{len(curr_obj_dis_cord)} cordinates list {len(curr_obj_pos_cord),type(curr_obj_dis_cord)}, ")
-            for coords in curr_obj_pos_cord:
-                x,y =coords
-                print(f"from visulize {x},{y}")
-            if(len(curr_obj_dis_cord)>0 and len(curr_obj_pos_cord)):
-                #print(f"    current displacement:\n{curr_obj_dis_cord}")
-                #print(f"    current origina cordinate:\n{curr_obj_pos_cord}")
-                curr_dis_arr = np.array(curr_obj_dis_cord)
-                curr_obs_arr = np.array(curr_obj_pos_cord)
-                #print(curr_dis_arr.shape,curr_obj_pos_cord)
+            # Update global min and max values
+            global_min_x = min(global_min_x, mu[0] - max_range)
+            global_max_x = max(global_max_x, mu[0] + max_range)
+            global_min_y = min(global_min_y, mu[1] - max_range)
+            global_max_y = max(global_max_y, mu[1] + max_range)
             
+    print(global_max_y,global_max_x)
+    print(global_min_y,global_max_y)
+    
+    for i, row_item in enumerate(grid_stat):
+        for j, col_item in enumerate(row_item):
+            mu=col_item['mu']
+            cov_matrix=col_item['cov_matrix']
             
-            if(len(curr_obj_dis_cord)>0):
-                mvn = multivariate_normal(mean=mu, cov=cov_matrix)
-                curr_pdf_values=mvn.pdf(curr_dis_arr)
-                
-                    
-    #print(len(pdfs_with_cords))          
-    return pdfs_with_cords
+            fig, ax = plt.subplots(figsize=(8, 8))
+
+            # Plot the mean as a point
+            ax.plot(mu[0], mu[1], 'ro', label="Mean")
+            
+            eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+            # Sort eigenvalues and eigenvectors
+            order = eigenvalues.argsort()[::-1]
+            eigenvalues, eigenvectors = eigenvalues[order], eigenvectors[:, order]
+    
+            # Calculate the angle of the ellipse
+            angle = np.degrees(np.arctan2(*eigenvectors[:, 0][::-1]))
+
+            # Width and height of the ellipse based on n_std standard deviations
+            width, height = 2 * 1.0 * np.sqrt(eigenvalues)
+
+            # Draw the ellipse
+            ellipse_1std = Ellipse(xy=mu, width=width, height=height, angle=angle,edgecolor='blue', linestyle='--', linewidth=2, facecolor='none', label="1 Std Dev")
+            ax.add_patch(ellipse_1std)
+            
+            ax.set_xlim(global_min_x, global_max_x)
+            ax.set_ylim(global_min_y, global_max_y)
+
+            # Set equal aspect ratio for both axes
+            ax.set_aspect('equal', adjustable='datalim')
+            
+            ax.set_xlabel("X")
+            ax.set_ylabel("Y")
+            ax.legend()
+            ax.grid(True)
+            ax.set_title(f"Visualization of Mean and Covariance Matrix as Ellipses For Grid {[i]}{[j]}")
+            #plt.axis('equal')
+            #plt.savefig(f"dead_grid_stat_[{i}][{j}]")
+            plt.show()
+
+def plot_cdf_line_side_by_side(dead_pdf_list, alive_pdf_list):
+    # Calculate log values of pdfs
+    #dead_log_values = np.log10(dead_pdf_list)
+    #alive_log_values= np.log10(alive_pdf_list)
+    
+    # Step 1: Sort the probability density values
+    dead_sorted_values = np.sort(dead_pdf_list)
+    alive_sorted_values = np.sort(alive_pdf_list)
+    # Step 2: Calculate the cumulative probabilities
+    dead_cdf = np.arange(1, len(dead_sorted_values) + 1) / len(dead_sorted_values)
+    alive_cdf = np.arange(1, len(alive_sorted_values) + 1) / len(alive_sorted_values)
+    
+    print("Range of PDF 1:", dead_sorted_values.min(), "to", dead_sorted_values.max())
+    print("Range of PDF 2:", alive_sorted_values.min(), "to", alive_sorted_values.max())
+    
+
+    # Step 3: Plot the CDFs side by side
+    fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+
+    # CDF for the first set of probability density values
+    axes[0].plot(dead_sorted_values, dead_cdf,color='blue')
+    axes[0].set_title('CDF of Dead PDF', fontsize=14)
+    axes[0].set_xlabel('Probability Density Values Dead', fontsize=12)
+    axes[0].set_ylabel('Cumulative Probability', fontsize=12)
+    axes[0].grid()
+
+    # CDF for the second set of probability density values
+    axes[1].plot(alive_sorted_values, alive_cdf,color='green')
+    axes[1].set_title('CDF of Alive PDF ', fontsize=14)
+    axes[1].set_xlabel('Probability Density Values Dead', fontsize=12)
+    axes[1].set_ylabel('Cumulative Probability', fontsize=12)
+    axes[1].grid()
+    
+    #plt.savefig(f"dead_alive_cdf_line.png", format='png', dpi=300)
+    # Adjust layout and display the plot
+    plt.tight_layout()
+    plt.show()
+    
+def plot_cdf_line_overlay(dead_pdf_list, alive_pdf_list):
+    # Calculate log values of pdfs
+    dead_log_values = np.log10(dead_pdf_list)
+    alive_log_values= np.log10(alive_pdf_list)
+    
+    # Step 1: Sort the probability density values
+    dead_sorted_values = np.sort(dead_log_values)
+    alive_sorted_values = np.sort(alive_log_values)
+    # Step 2: Calculate the cumulative probabilities
+    dead_cdf = np.arange(1, len(dead_sorted_values) + 1) / len(dead_sorted_values)
+    alive_cdf = np.arange(1, len(alive_sorted_values) + 1) / len(alive_sorted_values)
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(dead_sorted_values, dead_cdf,label='dead_pdfs', marker='.', linestyle='none', color='blue')
+    plt.plot(alive_sorted_values, alive_cdf,label='dead_pdfs', marker='.', linestyle='none', color='red')
+    #plt.plot(sorted_values, cdf, color='blue') #for line
+    plt.xlabel('Log Probability Density Values')
+    plt.ylabel('Cumulative Probability')
+    plt.title(f"Cumulative Distribution Function (CDF) For Dead(B) & Alive(R)")
+    #plt.savefig(f"overlay_dead_alive_cdf_scatter.png", format='png', dpi=300)
+    plt.grid()
+    plt.show()
+    
+def plot_cdf_min_max_normalized_line_overlay(dead_pdf_list, alive_pdf_list):
+    # Calculate log values of pdfs
+    dead_log_values = np.log10(dead_pdf_list)
+    alive_log_values= np.log10(alive_pdf_list)
+    
+    # Step 1: Normalize and Offset the Probability Density Values
+    norm_pdf_dead = (dead_pdf_list - min(dead_pdf_list)) / (max(dead_pdf_list) - min(dead_pdf_list))
+    norm_pdf_alive = (alive_pdf_list - min(alive_pdf_list)) / (max(alive_pdf_list) - min(alive_pdf_list)) + 0.5  # Adding offset to avoid overlap
+    
+    # Step 2: Add an offset to the second dataset's sorted values
+    dead_sorted_values = np.sort(norm_pdf_dead)
+    alive_sorted_values = np.sort(norm_pdf_alive)
+    
+    # Step 3: Calculate the cumulative probabilities
+    dead_cdf = np.arange(1, len(dead_sorted_values) + 1) / len(dead_sorted_values)
+    alive_cdf = np.arange(1, len(alive_sorted_values) + 1) / len(alive_sorted_values)
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(dead_sorted_values, dead_cdf,label='dead_pdfs', marker='.', linestyle='none', color='blue')
+    plt.plot(alive_sorted_values, alive_cdf,label='dead_pdfs', marker='.', linestyle='none', color='red')
+    #plt.plot(sorted_values, cdf, color='blue') #for line
+    plt.xlabel('Probability Density Values(Min-Max Normalized)')
+    plt.ylabel('Cumulative Probability')
+    plt.title(f"Cumulative Distribution Function (CDF) For Dead(B) & Alive(R) Offset 0.5")
+    #plt.savefig(f"overlay_dead_alive_cdf_scatter_min_max_noramlization.png", format='png', dpi=300)
+    plt.grid()
+    plt.show()
+
+def plot_cdf_zscore_normalized_line_overlay(dead_pdf_list, alive_pdf_list):
+    # Calculate log values of pdfs
+    dead_log_values = np.log10(dead_pdf_list)
+    alive_log_values= np.log10(alive_pdf_list)
+    
+    # Step 1: Normalize and Offset the Probability Density Values
+    zscore_pdf_dead = (dead_pdf_list - np.mean(dead_pdf_list)) / np.std(dead_pdf_list)
+    zscore_pdf_alive = (alive_pdf_list - np.mean(alive_pdf_list)) / np.std(alive_pdf_list)  # Adding offset to avoid overlap
+    
+    # Step 2: Add an offset to the second dataset's sorted values
+    dead_sorted_values = np.sort(zscore_pdf_dead)
+    alive_sorted_values = np.sort(zscore_pdf_alive+0.5)
+    
+    # Step 3: Calculate the cumulative probabilities
+    dead_cdf = np.arange(1, len(dead_sorted_values) + 1) / len(dead_sorted_values)
+    alive_cdf = np.arange(1, len(alive_sorted_values) + 1) / len(alive_sorted_values)
+    
+    #print("Range of PDF 1:", dead_sorted_values.min(), "to", dead_sorted_values.max())
+    #print("Range of PDF 2:", alive_sorted_values.min(), "to", alive_sorted_values.max())
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(dead_sorted_values, dead_cdf,label='dead_pdfs', marker='.', linestyle='none', color='blue')
+    plt.plot(alive_sorted_values, alive_cdf,label='dead_pdfs', marker='.', linestyle='none', color='red')
+    #plt.plot(sorted_values, cdf, color='blue') #for line
+    plt.xlabel('Probability Density Values(Normalized With Zscore)')
+    plt.ylabel('Cumulative Probability')
+    plt.title(f"Cumulative Distribution Function (CDF) For Dead(B) & Alive(R) Offset 0.5")
+    #plt.savefig(f"overlay_dead_alive_cdf_scatter_zscore_noramlization with offset.png", format='png', dpi=300)
+    plt.grid()
+    plt.show()